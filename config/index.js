a3dfdc9b17b5cbb5feba5db2af2a5f23d14f0f4f
Charles Hong <44939226+genkihong@users.noreply.github.com>
2019-03-18 18:34:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Lets-Cafe',
+    assetsPublicPath: '/Lets-Cafe/',
 
     /**
      * Source Maps
