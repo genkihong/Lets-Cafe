4b4b6171801e01496e72571eee99ea1a6ad6b720
Charles Hong <genkihong@gmail.com>
2019-03-18 18:18:55 +0800

    add index.html from dist folder

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',   //環境變數，一定要加雙引號"
-  API_PATH: '"https://vue-course-api.hexschool.io"',
-  CUSTOM_PATH:'"charles"'
-})
--- src/filters/date.js
+++
@@ -1,4 +0,0 @@
-export default function (time) {
-  const date = new Date(time * 1000);
-  return date.toLocaleDateString();
-}
